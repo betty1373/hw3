a7905af8df650869af5ad8bd88be38e794dbdd0d
betty1373 <61179097+betty1373@users.noreply.github.com>
2021-04-19 15:33:10 +0300

    Update test_version.cpp

--- test_version.cpp
+++ test_version.cpp
@@ -1,6 +1,6 @@
 #define BOOST_TEST_MODULE test_version
 
-#include "libSrc/lib.h"
+#include "lib.h"
 
 #include <boost/test/unit_test.hpp>
 
@@ -8,7 +8,7 @@ BOOST_AUTO_TEST_SUITE(test_version)
 
 BOOST_AUTO_TEST_CASE(test_valid_version)
 {
-    BOOST_CHECK(version() > 0);
+    BOOST_CHECK(libsrc::version() > 0);
 }
 
 }
