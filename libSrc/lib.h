34e4806bfcb7c2b394fcaa24be5adf214cda0029
betty1373 <61179097+betty1373@users.noreply.github.com>
2021-04-19 15:20:47 +0300

    Update lib.h

--- libSrc/lib.h
+++ libSrc/lib.h
@@ -1,1 +1,25 @@
+#pragma once
+#include <iostream>
+#include <cassert>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <algorithm>
 
+namespace libSrc 
+{
+    using ip_str_t = std::vector<std::string>;
+    using ip_t = std::vector<int>;
+    using ip_pool_t = std::vector<ip_t>;
+    ip_str_t split(const std::string &str, char d);
+    ip_t strToInt(const ip_str_t &str_ip);
+  
+    int version();
+
+    void sort(ip_pool_t &ip_pool); 
+    void print(const ip_pool_t &ip_pool); 
+
+    ip_pool_t filter(const ip_pool_t &ip_pool, int part); 
+    ip_pool_t filter(const ip_pool_t &ip_pool, int part1, int part2); 
+    ip_pool_t filter_any(const ip_pool_t &ip_pool, int part);
+}
