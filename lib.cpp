6fe8cbb174a3080e5aef08dc698af1cc4d988267
betty1373 <61179097+betty1373@users.noreply.github.com>
2021-04-19 15:14:40 +0300

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,92 +1,94 @@
 #include "version.h"
 #include "lib.h"
-
-int version()
+namespace libSrc 
 {
-    return PROJECT_VERSION_PATCH;
-}
-
-ip_str_t split(const std::string &str, char d)
-{
-    ip_str_t r;
-
-    std::string::size_type start = 0;
-    std::string::size_type stop = str.find_first_of(d);
-    while(stop != std::string::npos)
+    int version()
     {
-        r.push_back(str.substr(start, stop - start));
-
-        start = stop + 1;
-        stop = str.find_first_of(d, start);
+        return PROJECT_VERSION_PATCH;
     }
 
-    r.push_back(str.substr(start));
-
-    return r;
-}
-ip_t strToInt(const ip_str_t &str_ip)
-{
-    ip_t r;
-
-    for (auto &part :str_ip)
+    ip_str_t split(const std::string &str, char d)
     {
-        r.push_back(std::stoi(part));
-    }
+        ip_str_t r;
 
-    return r;
-}
-
-void sort(ip_pool_t &ip_pool)
-{
-    std::sort(ip_pool.begin(),ip_pool.end(),std::greater<ip_t>());
-}
-
-void print(const ip_pool_t &ip_pool)
-{
-    for(auto& ip : ip_pool)
-    {
-        for(auto& ip_part : ip)
+        std::string::size_type start = 0;
+        std::string::size_type stop = str.find_first_of(d);
+        while(stop != std::string::npos)
         {
-            if (&ip_part != &ip[0])
+            r.push_back(str.substr(start, stop - start));
+
+            start = stop + 1;
+            stop = str.find_first_of(d, start);
+        }
+
+        r.push_back(str.substr(start));
+
+        return r;
+    }
+    ip_t strToInt(const ip_str_t &str_ip)
+    {
+        ip_t r;
+
+        for (auto &part :str_ip)
+        {
+            r.push_back(std::stoi(part));
+        }
+
+        return r;
+    }
+
+    void sort(ip_pool_t &ip_pool)
+    {
+        std::sort(ip_pool.begin(),ip_pool.end(),std::greater<ip_t>());
+    }
+
+    void print(const ip_pool_t &ip_pool)
+    {
+        for(auto& ip : ip_pool)
+        {
+            for(auto& ip_part : ip)
             {
-                std::cout << ".";
+                if (&ip_part != &ip[0])
+                {
+                    std::cout << ".";
 
+                }
+                std::cout << ip_part;
             }
-            std::cout << ip_part;
+            std::cout << std::endl;
         }
-        std::cout << std::endl;
     }
-}
 
-ip_pool_t filter(const ip_pool_t &ip_pool, int part)
-{
-    ip_pool_t r;
+    ip_pool_t filter(const ip_pool_t &ip_pool, int part)
+    {
+        ip_pool_t r;
 
-    for(auto& ip : ip_pool)
-    {
-        if (ip[0]==part) r.push_back(ip);
-    }
-    return r;
-}
-ip_pool_t filter(const ip_pool_t &ip_pool, int part1, int part2)
-{
-    ip_pool_t r;
-
-    for(auto& ip : ip_pool)
-    {
-        if (ip[0]==part1 && ip[1]==part2) r.push_back(ip);
-    }
-    return r;
-}
-ip_pool_t filter_any(const ip_pool_t &ip_pool, int part)
-{
-    ip_pool_t r;
-    for(auto& ip : ip_pool)
-    {
-        for(auto& ip_part : ip)
+        for(auto& ip : ip_pool)
         {
-            if (ip_part == part) r.push_back(ip);
+            if (ip[0]==part) r.push_back(ip);
         }
+        return r;
     }
-    return r;
+    ip_pool_t filter(const ip_pool_t &ip_pool, int part1, int part2)
+    {
+        ip_pool_t r;
+
+        for(auto& ip : ip_pool)
+        {
+            if (ip[0]==part1 && ip[1]==part2) r.push_back(ip);
+        }
+        return r;
+    }
+    ip_pool_t filter_any(const ip_pool_t &ip_pool, int part)
+    {
+        ip_pool_t r;
+        for(auto& ip : ip_pool)
+        {
+            for(auto& ip_part : ip)
+            {
+                if (ip_part == part) r.push_back(ip);
+            }
+        }
+        return r;
+    }
 }
